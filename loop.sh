de71f78d70561430b2a8b6804b0336fccce0526d
PKXH <pkxh@earthlink.net>
2020-10-14 17:19:53 +0000

    quoted parameter to properly handle inputs with escaped whitespace

--- loop.sh
+++ loop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ffmpeg -re -stream_loop -1 -i $1 -vcodec libx264 -acodec aac -f flv rtmp://localhost/ingest/stream
+ffmpeg -re -stream_loop -1 -i "$1" -vcodec libx264 -acodec aac -f flv rtmp://localhost/ingest/stream
--- reel-time.sh
+++ reel-time.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ffmpeg -re -ss 1:00:00 -i $1 -vcodec libx264 -acodec aac -f flv rtmp://localhost/ingest/stream
+ffmpeg -re -ss 1:00:00 -i "$1" -vcodec libx264 -acodec aac -f flv rtmp://localhost/ingest/stream
